5107135e969d2f3bc4b405ceb5b2677dea6c3090
noranfox <sadpo5660@gmail.com>
2022-10-31 12:50:26 +0900

    Swift

---
+++ Fisrt.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+//string 문자열, charactor 문자
+type(of: "고양이")
+Array( "고양이")
+type(of: Array("고양이"))
+"""
+안녕하세요. 고양이입니다
+"""
+
+"안녕하세요. 테스트입니다"
+
+"고양이가 \"야옹\"하고울었다"
+//빈문자열만들기
+String()
+"고양이"+"야옹"
+"고양이는 야옹하고 운다".split(separator: "옹")
+"고양이".dropFirst()
+"고양이".dropFirst(3)
+"고양이".dropLast()
+"고양이".dropLast(3)
+"고양이".isEmpty
